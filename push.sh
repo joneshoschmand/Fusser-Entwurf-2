5ee58e48990891956f762b59c3e857d6f2d0e3fc
joneshoschmand <joneshoschmand@github>
2026-05-09 17:03:42 +0200

    Fussner Entwurf 2 - aktuelle Version

---
+++ push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd "/Users/joneshoschmand/Desktop/Webseiten/Fussner 3"
+git add .
+git commit -m "Fussner Entwurf 2 - aktuelle Version"
+git push -u origin main --force
+echo "Push abgeschlossen!"
